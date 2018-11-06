5dd0915ca5f87f021747dd396850b60eed719338
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-11-06 10:47:28 +0100

    add pinMessage to slack service and todoController

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,8 +4,15 @@ const slackService = require('../services/slack');
 exports.postTodoItem = async (ctx) => {
   const res = await slackService.postToSlack(channel, ctx.request.body.text);
   if (res.ok === true) {
-    ctx.body = 'Slash command completed.';
-    ctx.status = 200;
+    const result = await slackService.pinMessage(res);
+    if (res.ok === true) {
+      ctx.body = 'Slash command completed.';
+      ctx.status = 200;
+    } else {
+    // TODO: error message not working, fix it
+      ctx.body = result.error;
+      ctx.status = 503;
+    }
   } else {
     // TODO: error message not working, fix it
     ctx.body = res.error;
--- services/slack.js
+++ services/slack.js
@@ -6,3 +6,9 @@ const web = new WebClient(token);
 exports.postToSlack = (channel, text) => web.chat.postMessage({ channel, text })
   .then(res => res)
   .catch(err => err.data);
+
+exports.pinMessage = messageResponse => web.pins.add({
+  channel: messageResponse.channel,
+  timestamp: messageResponse.ts,
+}).then(res => res)
+  .catch(err => err.data);
